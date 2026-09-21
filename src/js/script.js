90b9d7b6a59c31909633af69be863285da3ad3ea
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-21 11:47:38 -0300

    Adicionando o Javascript

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+// DECLARAÇÃO
+var nome = "FIAP";
+console.log(typeof nome) // typeof antes da variável identifica a variável
+
+let idade = 19;
+console.log(idade)
+
+const dev = true;
+console.log(dev)
+
+let dados = ["um", "dois"]; // Array
+console.log(dados)
